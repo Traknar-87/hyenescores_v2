652a9441230480dbb4c7134ec92072c882d13615
Traknar-87 <traki.network@gmail.com>
2026-01-04 02:50:43 +0100

    Add src/App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import { Toaster } from "sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Index from "./pages/Index";
+import Match from "./pages/Match";
+import Palmares from "./pages/Palmares";
+import Pantheon from "./pages/Pantheon";
+import NotFound from "./pages/NotFound";
+
+const queryClient = new QueryClient();
+
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <TooltipProvider>
+      <Toaster />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Index />} />
+          <Route path="/match" element={<Match />} />
+          <Route path="/palmares" element={<Palmares />} />
+          <Route path="/pantheon" element={<Pantheon />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </TooltipProvider>
+  </QueryClientProvider>
+);
+
+export default App;
